61233fe94c484946ad46d8db1eb8ca1424182880
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-09 18:06:03 +0200

    feat: added dinamic cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home page</h1>
+    <section>
+        <div class="container">
+            <div class="row g-3">
+                @foreach ($products as $card)
+                    <div class="col-12 col-md-6 col-lg-4 mb-4">
+                        <div class="ms_card h-100">
+                            <img src="{{ Vite::asset('resources/img/' . $card['frontImage']) }}"" alt="">
+                            <div class="ms_card-body">
+                                <p>
+                                    {{ $card['name'] }}
+                                </p>
+                                <p>
+                                    {{ $card['brand'] }}
+                                </p>
+                                <p>
+                                    {{ $card['price'] }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
